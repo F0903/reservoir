01a7c4d94a844c2a54934b5fb4e18c39da8f1812
Frederik P. <frepbh@gmail.com>
2025-11-09 17:51:37 +0100

    Refactored the atomics int64 and uin64 wrappers

--- utils/atomics/uint64.go
+++ utils/atomics/uint64.go
@@ -5,62 +5,62 @@ import (
 	"sync/atomic"
 )
 
-// Int64 is a wrapper around the Go 1.19+ atomic.Int64 type,
+// Uint64 is a wrapper around the Go 1.19+ atomic.Uint64 type,
 // providing convenience methods and JSON serialization.
 // The struct holds a pointer to the atomic value, making the wrapper itself copy-safe.
-type Int64 struct {
-	val *atomic.Int64
+type Uint64 struct {
+	val *atomic.Uint64
 }
 
-// NewInt64 creates a new Int64 with an initial value.
-func NewInt64(initialValue int64) Int64 {
-	val := &atomic.Int64{}
+// NewUint64 creates a new Uint64 with an initial value.
+func NewUint64(initialValue uint64) Uint64 {
+	val := &atomic.Uint64{}
 	val.Store(initialValue)
-	return Int64{val: val}
+	return Uint64{val: val}
 }
 
 // Add atomically adds delta to the value.
-func (i *Int64) Add(delta int64) {
-	i.val.Add(delta)
+func (u *Uint64) Add(delta uint64) {
+	u.val.Add(delta)
 }
 
 // Sub atomically subtracts delta from the value.
-func (i *Int64) Sub(delta int64) {
-	i.val.Add(-delta)
+func (u *Uint64) Sub(delta uint64) {
+	u.val.Add(^uint64(delta - 1))
 }
 
 // Increment atomically increments the value by 1.
-func (i *Int64) Increment() {
-	i.Add(1)
+func (u *Uint64) Increment() {
+	u.Add(1)
 }
 
 // Decrement atomically decrements the value by 1.
-func (i *Int64) Decrement() {
-	i.Sub(1)
+func (u *Uint64) Decrement() {
+	u.Sub(1)
 }
 
 // Set atomically sets the value.
-func (i *Int64) Set(value int64) {
-	i.val.Store(value)
+func (u *Uint64) Set(value uint64) {
+	u.val.Store(value)
 }
 
 // Get atomically retrieves the value.
-func (i *Int64) Get() int64 {
-	return i.val.Load()
+func (u *Uint64) Get() uint64 {
+	return u.val.Load()
 }
 
 // MarshalJSON implements the json.Marshaler interface with a value receiver.
 // This is safe because the struct itself is copy-safe (it only contains a pointer).
-func (i Int64) MarshalJSON() ([]byte, error) {
-	return json.Marshal(i.Get())
+func (u Uint64) MarshalJSON() ([]byte, error) {
+	return json.Marshal(u.Get())
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface.
-func (i *Int64) UnmarshalJSON(data []byte) error {
-	var value int64
+func (u *Uint64) UnmarshalJSON(data []byte) error {
+	var value uint64
 	if err := json.Unmarshal(data, &value); err != nil {
 		return err
 	}
-	i.Set(value)
+	u.Set(value)
 	return nil
 }
